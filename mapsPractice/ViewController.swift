84b3ef1867d97d2320769c7a937f4e24b12b8d86
Sreejita Patra <sreejitapatra11@gmail.com>
2022-08-03 09:33:55 -0400

    map for a city of unfortunate events

--- mapsPractice/ViewController.swift
+++ mapsPractice/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var MapView:
+        MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let initialLocation =
+        CLLocation (latitude:40.7128, longitude: 74.0060)
+        
+        MapView.centerToLocation(initialLocation)
+
         // Do any additional setup after loading the view.
+        
+    }
+    private extension MKMapView {
+      func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1000
+      ) {
+        let coordinateRegion = MKCoordinateRegion(
+          center: location.coordinate,
+          latitudinalMeters: regionRadius,
+          longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+      }
     }
 
 
